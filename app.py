fd3fd405193135dd030fc7c0481ab6fcb4d0e5d6
mahirhkhan <mkhan18@u.rochester.edu>
2017-06-28 15:07:52 -0700

    still broken, but less broken now

--- app.py
+++ app.py
@@ -4,30 +4,12 @@ from flask import request
 import os
 
 mse_folder = "temp/"
-allowed_extensions = set(['txt'])
 
 app = Flask(__name__)
 app.config['upload_folder'] = mse_folder
 
-def allowed_file(filename):
-    if '.' in filename and filename.rsplit('.',1).lower() in allowed_extensions:
-        return True
-    else:
-        return False
-
 @app.route('/',methods=['GET','POST'])
-def index():
-    if request.method == 'POST':
-        if 'mses' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        textfile = request.files['mses']
-        if textfile == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if textfile and allowed_file(textfile):
-            textfile_name = secure_filename(textfile.filename)
-            textfile.save(os.path.join(app.config['upload_folder'], textfile_name))
+def index():   
     with open('static/index.html') as html_code:
         html_text = html_code.readlines()
     my_html = ''.join(html_text)
@@ -63,6 +45,19 @@ def save_data():
     print(data)
     #do something with the data here -- save it to a file? 
     return "Success! This message is from the server"
+
+@app.route("/saveText/", methods=['POST'])
+def save_text():
+    textfile = request.files['mses']
+    test = False
+    if textfile.filename != "":
+        test = True
+    #if textfile == '':
+    #    return redirect(request.url)
+    #else:
+    #    textfile_name = textfile.filename
+    #    textfile.save(os.path.join(app.config['upload_folder'], textfile_name))
+    return test
 
 if __name__ == "__main__":
     app.run(port=1116)
